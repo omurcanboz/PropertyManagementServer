878b8310225db65b6acaad63a912f21fff4b5d38
Omur Can Boz <omur.boz@i2i-systems.com>
2022-05-13 00:21:23 -0500

    *top 10 rented property has been added

--- src/main/java/com/project/accessdenied/service/impl/PropertyServiceImpl.java
+++ src/main/java/com/project/accessdenied/service/impl/PropertyServiceImpl.java
@@ -82,7 +82,7 @@ public class PropertyServiceImpl implements PropertyService {
             }
         }).limit(10).collect(Collectors.toList());
 */
-        return propertyRepository.findTop10ByOrderByLastRentedDateDesc();
+        return propertyRepository.findTop10ByNameOrderByLastRentedDateDesc();
     }
 
     @Override
